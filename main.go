b7e331705143162a830051cbafa136d8b9df605f
girigirig <gideongirigiri@gmail.com>
2020-03-28 21:19:38 -0400

    initial commit

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	var name string
+	fmt.Print("Project Name: ")
+	fmt.Scan(&name)
+
+	generate(name)
+}
+
+func generate(name string) {
+	directoriesToMake := []string{"cmd", "internal", "pkg", "vendor", "api", "web", "config", "init", "script", "build", "deployments", "test"}
+
+	for _, dir := range directoriesToMake {
+		os.Mkdir(name, 0777)
+
+		if _, err := os.Stat(dir); os.IsNotExist(err) {
+
+			if err := os.Mkdir(name+"/"+dir, 0777); err != nil {
+				fmt.Println(err.Error())
+			}
+
+			if dir == "cmd" {
+				f, _ := os.Create(name + "/" + "cmd/main.go")
+				defer f.Close()
+				f.WriteString("package main\n\n\nfunc main() {\n\n}")
+			}
+		}
+
+	}
+}
